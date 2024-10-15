82416cb0c8264236f9f225bb4270d6487cb89b8e
iamnana-tyc <nanatyc7@gmail.com>
2024-10-15 09:16:58 +0500

    implement auth service

---
+++ src/main/java/com/iamnana/movieApi/auth/service/AuthService.java
@@ -0,0 +1,61 @@
+package com.iamnana.movieApi.auth.service;
+
+import com.iamnana.movieApi.auth.entities.User;
+import com.iamnana.movieApi.auth.entities.UserRole;
+import com.iamnana.movieApi.auth.repository.UserRepository;
+import com.iamnana.movieApi.auth.utils.AuthResponse;
+import com.iamnana.movieApi.auth.utils.LoginRequest;
+import com.iamnana.movieApi.auth.utils.RegisterRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final RefreshTokenService refreshTokenService;
+    private final AuthenticationManager authenticationManager;
+
+
+    public AuthResponse register(RegisterRequest registerRequest){
+        var user = User.builder()
+                .name(registerRequest.getName())
+                .email(registerRequest.getEmail())
+                .username(registerRequest.getUsername())
+                .password(passwordEncoder.encode(registerRequest.getPassword()))
+                .role(UserRole.USER)
+                .build();
+
+        User savedUser =  userRepository.save(user);
+        var accessToken = jwtService.generateToken(savedUser);
+        var refreshToken = refreshTokenService.createRefreshToken(savedUser.getEmail());
+
+        return AuthResponse.builder()
+                .accessToken(accessToken)
+                .refreshToken(refreshToken.getRefreshToken())
+                .build();
+    }
+
+    public AuthResponse login(LoginRequest loginRequest){
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getEmail(),
+                        loginRequest.getPassword()
+                )
+        );
+        var user = userRepository.findByEmail(loginRequest.getEmail()).orElseThrow(()-> new UsernameNotFoundException("The user doesn't exist!"));
+        var accessToken = jwtService.generateToken(user);
+        var refreshToken = refreshTokenService.createRefreshToken(loginRequest.getEmail());
+
+        return AuthResponse.builder()
+                .accessToken(accessToken)
+                .refreshToken(refreshToken.getRefreshToken())
+                .build();
+    }
+}
