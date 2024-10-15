a1500157bd7d46aeeb83b565bb96034dddb1d211
iamnana-tyc <nanatyc7@gmail.com>
2024-10-15 09:20:33 +0500

    class to register user

---
+++ src/main/java/com/iamnana/movieApi/auth/utils/RegisterRequest.java
@@ -0,0 +1,17 @@
+package com.iamnana.movieApi.auth.utils;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class RegisterRequest {
+    private String name;
+    private String username;
+    private String email;
+    private String password;
+}
